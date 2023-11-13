f9eed2ad0f23238473b89807eaf1e6c39ad73102
Eric Yu <z333yu@uwaterloo.ca>
2023-11-13 01:33:49 +0000

    Results on ArchiveII_all, Task: Secondary Structure pred, check results in recover_experiments/secondary_structure.ipynb

--- recover_experiments/secondary_structure.py
+++
@@ -1,8 +0,0 @@
-import fm
-import torch
-from ..redevelop.data import make_data_loader
-
-if __name__ == "__main__":
-    model, alphabet = fm.downstream.build_rnafm_resnet(type="ss") 
-    train_loader, val_loader, test_loader = make_data_loader()
-    # for
